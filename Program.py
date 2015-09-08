e2d77197a0d7caaa6aca31a2cb2af635cb1c45ef
Dany Sousa <danysousa.sd@gmail.com>
2015-09-08 11:43:28 +0200

    [ADD] Program Class

---
+++ Program.py
@@ -0,0 +1,12 @@
+class Program(object):
+
+	def __init__(self, name, config):
+		self.name = name
+		self.config = config
+		# print(name)
+
+		# if ( "autostart" in self.config and self.config["autostart"] == True ) :
+		# 	print("YOLO")
+		# else :
+		# 	print(self.config["autostart"])
+
--- main.py
+++ main.py
@@ -3,49 +3,56 @@ import getopt
 import os
 import json
 from pprint import pprint
+from Program import Program
 
-def variableEnvExist( name ):
-	for value in os.environ:
-		if ( name == value ):
-			return ( 1 )
-	return ( 0 )
+# def variableEnvExist( name ):
+# 	for value in os.environ:
+# 		if ( name == value ):
+# 			return ( 1 )
+# 	return ( 0 )
 
-def setVariableEnv( name, value ):
-	if ( variableEnvExist( name ) == 1 ):
-		os.environ[name] = os.environ[name] + value
-	else:
-		os.environ[name] = value;
+# def setVariableEnv( name, value ):
+# 	if ( variableEnvExist( name ) == 1 ):
+# 		os.environ[name] = os.environ[name] + value
+# 	else:
+# 		os.environ[name] = value;
 
-def showVariableEnv():
-	for value in os.environ:
-		print( value + " = " + os.environ[value] )
-	print("---------------------")
-	os.system( 'env | grep POK' )
+# def showVariableEnv():
+# 	for value in os.environ:
+# 		print( value + " = " + os.environ[value] )
+# 	print("---------------------")
+# 	os.system( 'env | grep POK' )
 
-def getNextLine( file ):
-	f = open( argv[1], 'r' )
-	lignes  = f.readlines()
-	f.close()
+# def getNextLine( file ):
+# 	f = open( argv[1], 'r' )
+# 	lignes  = f.readlines()
+# 	f.close()
 
-	for ligne in lignes:
-		print( ligne )
+# 	for ligne in lignes:
+# 		print( ligne )
 
 def parsing( file ):
 	with open(file) as data_file:
 		data = json.load(data_file)
 
-	print(data["glossary"]["title"])
+	return data
 
 def main( argv ):
-	if ( len( argv ) == 2 ):
-		parsing(argv[1])
-		# setVariableEnv( "POK", "POUUUKKY POUUUKY");
-		# showVariableEnv()
+	if ( len( argv ) != 2 ):
+		return
 
-		# os.umask( 0o700 )
-		# fh1 = os.open( "qq1.junk", os.O_CREAT, 0o777 )
-		# os.close ( fh1 )
+	config = parsing(argv[1])
+	program = {};
+	for (key, value) in config.items():
+		program[key] = Program(key, value)
 
-		sys.exit( 0 );
+
+	# setVariableEnv( "POK", "POUUUKKY POUUUKY");
+	# showVariableEnv()
+
+	# os.umask( 0o700 )
+	# fh1 = os.open( "qq1.junk", os.O_CREAT, 0o777 )
+	# os.close ( fh1 )
+
 
 main( sys.argv )
