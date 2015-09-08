a3db4672798f0d5a56d21ddbfac2d7c439796ac0
Dany Sousa <danysousa.sd@gmail.com>
2015-09-08 15:36:01 +0200

    [CHANGE] Main class with shell && thread

--- Taskmaster.py
+++ Taskmaster.py
@@ -1,15 +1,34 @@
 from Program import Program
 import time
 import sys
-
+import os
+import threading
+import signal
 
 class Taskmaster(object):
 
 	def __init__(self, prog):
+		signal.signal(signal.SIGINT, self.quitBySignal)
 		self.prog = prog
-		self.start()
+		self.updated = 0;
+		self.isDone = False;
 
-	def start(self):
-		isDone = False
-		while ( isDone == False ):
+		self.t = threading.Thread(name='shell', target=self.shell)
+		self.t.start()
+		self.updateAll()
+
+	def quitBySignal(self, a, b):
+		self.isDone = True
+		print("Press return for quit")
+
+	def shell(self):
+		while ( self.isDone == False ):
+			line = input("$>")
+			print(self.updated)
+			if ( line == "exit" ) :
+				self.isDone = True
+
+	def updateAll(self):
+		while ( self.isDone == False ):
+			self.updated += 1
 			time.sleep(0.05)
